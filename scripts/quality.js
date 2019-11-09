49aec2f09e7411e8a5ea51a475ed7e7bfaee2679
1ak31sha <lakeisha.pogrzeba@gmail.com>
2019-11-09 14:15:41 -0500

    add node.js imgquality script

--- scripts/quality.js
+++ scripts/quality.js
@@ -1,3 +1,12 @@
 const exec = require('child_process').execSync
-const imgs = exec('find . -size +2M').toString()
-console.log(imgs)
+let imgs = exec('find ../source/images -size +2M').toString()
+ // console.log(imgs)
+// magick -quality 20 halwa2.jpg halwa2.jpg
+
+console.log(imgs = imgs.split('\n'))
+console.log(typeof(imgs))
+  imgs.map( (item) =>{
+  console.log(item)
+  exec(`magick -quality 20 ${item} ${item}`)
+  })
+
